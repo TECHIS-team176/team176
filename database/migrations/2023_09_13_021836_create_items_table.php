cc714e6a45d06fba1b0ccc88a58731254ee5cf1b
IvyChen <s335039837@gmail.com>
2023-09-13 02:22:59 +0900

    made items table

---
+++ database/migrations/2023_09_13_021836_create_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->string('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
