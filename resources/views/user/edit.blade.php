161d453cf551aec8f9aabca8d34078bac7e6b2fe
User <user@yuzanoMBP.lan>
2023-09-14 22:34:41 +0900

    230914FK

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>ユーザー編集画面</title>
+ </head>
+ <body>
+    <h1>ユーザー編集画面</h1>
+ </body>
+ </html>
